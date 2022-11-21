4f4536aa2e8fb9405aab89dbb30c84739699873a
mchib031 <71799789+mchib031@users.noreply.github.com>
2022-11-21 10:46:13 -0500

    hello world component implementation
    app-helloworld-component

--- Testing/testing-app/src/app/helloworld-component/helloworld-component.component.ts
+++ Testing/testing-app/src/app/helloworld-component/helloworld-component.component.ts
@@ -2,10 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-helloworld-component',
+  //use selector in app.component.html to show component
   templateUrl: './helloworld-component.component.html',
+  //template propriety if you're using a template within this file
   styleUrls: ['./helloworld-component.component.css']
+  //styles propriety if you're using a template within this file
 })
 export class HelloworldComponentComponent implements OnInit {
+
+  // public proprieties that you define in this class are accessible in the html template
+  title = 'Hello World Component';
 
   constructor() { }
 
