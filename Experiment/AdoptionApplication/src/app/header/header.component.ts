21d4de9c1c7f26c502be85177a2ace46ff7ca6f0
Mahdi Chiboub <71799789+mchib031@users.noreply.github.com>
2022-12-01 10:45:38 -0500

    experiment header component

---
+++ Experiment/AdoptionApplication/src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  followingToolbar:boolean = false;
+
+  @HostListener("document:scroll")
+  onScroll() {
+    if(document.documentElement.scrollTop > 0){
+      this.followingToolbar = true;
+    }
+    else {
+      this.followingToolbar = false;
+    }
+  }
+}
